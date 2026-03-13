046e3577461e00014ed4af0bd6bb95e2416f68ec
JacobM <jrmccormick2007@icloud.com>
2026-03-13 15:18:42 +0000

    I added some nav2 costmap functionality

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+import os
+from glob import glob
 package_name = 'galaxsea'
 
 setup(
@@ -9,6 +10,8 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
